7a05aab7ad73da5bde3b2f3561e7f1fcdf65d856
cupatea <vdavyden@gmail.com>
2017-10-26 19:12:06 +0300

    Add model tests

--- test/models/banknote_test.rb
+++ test/models/banknote_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class BanknoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should calculate total ' do
+    Banknote.update_all quantity: 0
+    assert Banknote.total.zero?
+    Banknote.update_all quantity: 1
+    assert_equal Banknote.kinds.values.sum, Banknote.total
+  end
 end
--- test/models/transaction_test.rb
+++ test/models/transaction_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class TransactionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should calculate amount after refill' do
+    transaction = Transaction.create kind: 'refill', banknotes: { 1 => 5, 2 => 10 }
+    assert_equal 25, transaction.amount
+  end
 end
